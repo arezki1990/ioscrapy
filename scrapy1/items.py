1d90966a0036a586ed84af400e4f0f5974c7ac7c
arezki1990 <a_lebdiri@esi.dz>
2014-05-13 13:33:28 -0100

    update items.py
    pour amazone

--- scrapy1/items.py
+++ scrapy1/items.py
@@ -2,26 +2,23 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
-
 from scrapy.item import Item, Field
 
-class ItemImmobilier(Item):
-	date=Field()
+class ImmobilierItem(Item):
 	id=Field()
-	url=Field()
 	name=Field()
-	category=Field()
-	description=Field()
-	contrat=Field()
-	nombre_de_piece=Field()
-	superficie=Field()
-	specification=Field()
-	nombre_d_etage=Field()
 	prix=Field()
+	category=Field()
+	wilaya=Field()
+	nbr_piece=Field()
+	nbr_etage=Field()
 	description=Field()
-	images=Field()
-class ItemInformatique(Item):
-	id=Field()
+	surface=Field()
+	url=Field()
+	date=Field()
+	ville=Field()
+	source=Field()
+	image=Field()
 
 
 

1447680827154c6904ea9139a8dc24104fc80ee5
arezki1990 <a_lebdiri@esi.dz>
2014-05-13 13:43:55 -0100

    Update dlalaonline.py

--- scrapy1/spiders/dlalaonline.py
+++ scrapy1/spiders/dlalaonline.py
@@ -1,7 +1,7 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
-from scray_project.items import ImmobilierItem
+from scrapy1.items import ImmobilierItem
 from scrapy.contrib.loader import ItemLoader
 
 class MySpider(CrawlSpider):

679ef31e55d23ddde263b4dd4ebab6e767359d01
arezki1990 <a_lebdiri@esi.dz>
2014-05-13 13:53:55 -0100

    Update spider1.py

--- scrapy1/spiders/spider1.py
+++ scrapy1/spiders/spider1.py
@@ -1,7 +1,7 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
-from scrapy1.items import ItemImmobilier
+from scrapy1.items import ImmobilierItem
 
 class MySpider(CrawlSpider):
     name = 'immobilier'
@@ -15,20 +15,9 @@ class MySpider(CrawlSpider):
 
     def parse_item(self, response):
     	sel=Selector(response)
-        item=ItemImmobilier()
+        item=ImmobilierItem()
         item["url"]=response.url
-        item["name"]=sel.xpath('//div[@id="annonce"]/h1[@id="Title"]/text()').extract()
-        item["category"]=sel.xpath('//div[@id="annonce"]/div[@id="Description"]/p[re:test(@id, "Cat\w.*$")]/span/text()').extract()
-        item["contrat"]=sel.xpath('//div[@id="annonce"]/div[@id="Description"]/p[re:test(@id, "Dur\we minimale du contrat$")]/span/text()').extract()
-        item["nombre_de_piece"]=sel.xpath('//div[@id="annonce"]/div[@id="Description"]/p[re:test(@id, "Nombre de pi\wces$")]/span/text()').extract()
-        item["superficie"]=sel.xpath('//div[@id="annonce"]/div[@id="Description"]/p[re:test(@id, "Nombre de pi\wces$")]/span/text()').extract()
-        item["nombre_d_etage"]=sel.xpath('//div[@id="annonce"]/div[@id="Description"]/p[re:test(@id, "Nombre\.*tage$")]/span/text()').extract()
-        item["prix"]=sel.xpath('//*[@id="Prix"]/span/text()').extract()
-        item["specification"]=sel.xpath('//div[@id="annonce"]/div[@id="Description"]/psel[re:test(@id, "Sp\wcifications$")]/span/text()').extract()
-        item["date"]=sel.xpath('//*[@id="menu"]/div/span[3]/text()').extract()
-        item["description"]=sel.xpath('//*[@id="GetDescription"]/text()').extract()
-        item["images"]=sel.xpath('//*[@id="gallery"]/a[1]/img/@src').extract()
-        url_image=[]
+
         # for i in img:#Nombre\20 d
         # 	url_image.append(i.xpath('/@src').extract())
         
